4b4cf448fcfbf29435a461a9a4421e424a4a0bd6
Mohamed Helmi <mtabila37@gmail.com>
2025-03-09 01:36:31 +0200

    update pet profile screen to integrate the pet service and add tabs for logs.

--- screens/profiles/SingleProfile.screen.tsx
+++ screens/profiles/SingleProfile.screen.tsx
@@ -1,39 +1,13 @@
-import React, { useEffect, useState } from 'react';
-import {
-  View,
-  Text,
-  StyleSheet,
-  ScrollView,
-  ActivityIndicator,
-} from 'react-native';
-import { NativeStackScreenProps } from '@react-navigation/native-stack';
-import { Pet, BodyConditionLog, WeightLog } from '../../types';
+import React, { useEffect, useMemo, useState } from 'react';
+import { View, Text, StyleSheet, ScrollView, ActivityIndicator, TouchableOpacity, } from 'react-native';
+import { Pet, BodyConditionLog, WeightLog, AppScreenProps, VetVisitLog } from '../../types';
+import { supabase } from '../../services';
 
-type RootStackParamList = {
-  SingleProfile: { id: string };
-};
-
-type Props = NativeStackScreenProps<RootStackParamList, 'SingleProfile'>;
-
-// Mock data for development
-const mockPet: Pet = {
-  id: '1',
-  name: 'Max',
-  species: 'Dog',
-  breed: 'Golden Retriever',
-  age: 3,
-  created_at: new Date().toISOString(),
-  owner_id: '123',
-  logs_weight: [
-    { id: '1', pet_id: '1', weight: 25.5, date: '2024-02-25T10:00:00Z' },
-    { id: '2', pet_id: '1', weight: 26.0, date: '2024-01-25T10:00:00Z' },
-  ],
-  logs_bodycondition: [
-    { id: '1', pet_id: '1', body_condition: "3", date: '2024-02-25T10:00:00Z' },
-    { id: '2', pet_id: '1', body_condition: "4", date: '2024-01-25T10:00:00Z' },
-  ],
-  logs_vet_visits: [],
-};
+const TABS = [
+  {id: 'Weight_Logs', name: 'Weight Logs'},
+  {id: 'Body_Condition', name: 'Body Condition'},
+  {id: 'Vet_Visits', name: 'Vet Visits'},
+]
 
 function getThisMonthLogs(logs_bodycondition: BodyConditionLog[], logs_weight: WeightLog[]) {
   const currentMonth = new Date().getMonth();
@@ -42,18 +16,18 @@ function getThisMonthLogs(logs_bodycondition: BodyConditionLog[], logs_weight: W
   const latestBodyConditionLog = logs_bodycondition
     .filter(
       (log) =>
-        new Date(log.date).getMonth() === currentMonth &&
-        new Date(log.date).getFullYear() === currentYear
+        new Date(log?.date).getMonth() === currentMonth &&
+        new Date(log?.date).getFullYear() === currentYear
     )
     .sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime())[0];
 
   const latestWeightLog = logs_weight
     .filter(
       (log) =>
-        new Date(log.date).getMonth() === currentMonth &&
-        new Date(log.date).getFullYear() === currentYear
+        new Date(log?.date).getMonth() === currentMonth &&
+        new Date(log?.date).getFullYear() === currentYear
     )
-    .sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime())[0];
+    .sort((a, b) => new Date(b?.date).getTime() - new Date(a?.date).getTime())?.[0];
 
   return { latestBodyConditionLog, latestWeightLog };
 }
@@ -66,36 +40,80 @@ const PetCard = ({ pet }: { pet: Pet }) => (
   </View>
 );
 
-const LogsTable = ({ 
-  weightLogs, 
-  bodyConditionLogs 
-}: { 
-  weightLogs: WeightLog[], 
-  bodyConditionLogs: BodyConditionLog[] 
-}) => (
-  <View style={styles.table}>
-    <Text style={styles.tableHeader}>Recent Logs</Text>
-    {weightLogs.map((log, index) => (
-      <View key={index} style={styles.tableRow}>
-        <Text>Weight: {log.weight}kg</Text>
-        <Text>Date: {new Date(log.date).toLocaleDateString()}</Text>
+const LogsTable = ({ data, }: { data: {id: string; name: string; date: string}[], }) => {
+
+  if(data?.length == 0) {
+    return (
+      <View style={styles.table}>
+        <Text>There are no logs</Text>
       </View>
-    ))}
-  </View>
-);
+    )
+  }
+
+  return (
+    <View style={styles.table}>
+      {data?.map((log, index) => (
+        <View key={index} style={styles.tableRow}>
+          <Text>{log?.name}</Text>
+          <Text>{log?.date}</Text>
+        </View>
+      ))}
+    </View>
+  )
+};
+
+const VetVisitsTable = ({ data, onAddNew}: { data: VetVisitLog[], onAddNew: () => void}) => {
+
+  if(data?.length == 0) {
+    return (
+      <View style={styles.table}>
+        <Text>There are no logs</Text>
+      </View>
+    )
+  }
+
+  return (
+    <View style={styles.table}>
+      {data?.map((log, index) => (
+        <View key={index} style={styles.vet_visit_card}>
+          <Text>{log?.notes}</Text>
+          <Text>{new Date(log?.date)?.toLocaleDateString()}</Text>
+        </View>
+      ))}
+
+      <TouchableOpacity activeOpacity={.8} onPress={onAddNew} style={styles.button}>
+        <Text style={styles.button_text}>
+          Add new vet visit
+        </Text>
+      </TouchableOpacity>
+    </View>
+  )
+};
 
 const HealthStatus = ({ pet }: { pet: Pet }) => (
   <View style={styles.healthStatus}>
     <Text style={styles.tableHeader}>Health Status</Text>
-    <Text>Overall Health: {pet?.logs_weight.length > 3 ? 'Good' : 'Needs More Data'}</Text>
+    <Text>Overall Health: {pet?.logs_weight?.length > 3 ? 'Good' : 'Needs More Data'}</Text>
     <Text>Last Vet Visit: 2 months ago</Text>
   </View>
 );
 
-export const SingleProfileScreen: React.FC<Props> = ({ route }) => {
+export const SingleProfileScreen: React.FC<AppScreenProps<'SingleProfile'>> = ({ route }) => {
   const { id } = route.params;
-  const [pet, setPet] = useState<Pet | null>(null);
   const [loading, setLoading] = useState(true);
+  const [tab, setTab] = useState(TABS?.[0]?.id); 
+  const [error, setError] = useState<string | null>(null);
+  const [petData, setPetData] = useState<{
+    pet: Pet | null;
+    weightLogs: WeightLog[];
+    bodyConditionLogs: BodyConditionLog[];
+    vetVisitLogs: VetVisitLog[];
+  }>({
+    pet: null,
+    weightLogs: [],
+    bodyConditionLogs: [],
+    vetVisitLogs: []
+  });
   const [thisMonthLogs, setThisMonthLogs] = useState<{
     latestBodyConditionLog: BodyConditionLog | null;
     latestWeightLog: WeightLog | null;
@@ -104,31 +122,52 @@ export const SingleProfileScreen: React.FC<Props> = ({ route }) => {
     latestWeightLog: null,
   });
 
+  const tab_obj = useMemo(() => {
+    return TABS?.find(e => e?.id === tab);
+  }, [tab]);
+
+
   useEffect(() => {
-    const fetchPet = async () => {
+    const fetchPetData = async () => {
+      setLoading(true);
       try {
-        // Simulate network delay
-        await new Promise(resolve => setTimeout(resolve, 1000));
-        setPet(mockPet);
+        const [petResult, weightResult, bodyConditionResult, vetVisitResult] = await Promise.all([
+          supabase.from('pets').select().eq('id', id).single(),
+          supabase.from('weight_logs').select().eq('pet_id', id),
+          supabase.from('body_condition_logs').select().eq('pet_id', id),
+          supabase.from('vet_visit_logs').select().eq('pet_id', id)
+        ]);
+
+        if (petResult.error) throw new Error(petResult?.error?.message);
+        
+        setPetData({
+          pet: petResult?.data || null,
+          weightLogs: weightResult?.data || [],
+          bodyConditionLogs: bodyConditionResult?.data || [],
+          vetVisitLogs: vetVisitResult?.data || []
+        });
+      } catch (err: any) {
+        console.error('Error fetching pet data:', err?.message);
+        setError(err.message);
       } finally {
         setLoading(false);
       }
     };
 
-    fetchPet();
+    fetchPetData();
   }, [id]);
 
   useEffect(() => {
-    if (pet) {
-      setThisMonthLogs(getThisMonthLogs(pet.logs_bodycondition, pet.logs_weight));
+    if (petData?.weightLogs && petData?.bodyConditionLogs) {
+      setThisMonthLogs(getThisMonthLogs(petData?.bodyConditionLogs, petData?.weightLogs));
     }
-  }, [pet]);
+  }, [petData?.vetVisitLogs, petData?.bodyConditionLogs]);
 
   if (loading) {
     return <ActivityIndicator style={styles.loader} />;
   }
 
-  if (!pet) {
+  if (error || !petData?.pet) {
     return (
       <View style={styles.container}>
         <Text>Pet not found</Text>
@@ -137,25 +176,34 @@ export const SingleProfileScreen: React.FC<Props> = ({ route }) => {
   }
 
   return (
-    <ScrollView style={styles.container}>
-      <PetCard pet={pet} />
+    <ScrollView style={styles.container} contentContainerStyle={styles.scroll_content}>
+      <PetCard pet={petData?.pet} />
       
       <View style={styles.monthSummary}>
         <Text style={styles.tableHeader}>This Month's Summary</Text>
         <Text>
-          Latest Weight: {thisMonthLogs.latestWeightLog?.weight || 'No data'} kg
+          Latest Weight: {thisMonthLogs?.latestWeightLog?.weight || 'No data'} kg
         </Text>
         <Text>
-          Body Condition: {thisMonthLogs.latestBodyConditionLog?.body_condition || 'No data'}
+          Body Condition: {thisMonthLogs?.latestBodyConditionLog?.body_condition || 'No data'}
         </Text>
       </View>
 
-      <HealthStatus pet={pet} />
+      <HealthStatus pet={petData?.pet} />
       
-      <LogsTable 
-        weightLogs={pet.logs_weight} 
-        bodyConditionLogs={pet.logs_bodycondition} 
-      />
+      <View style={styles.tabs}>
+        {TABS?.map(e => (
+          <TouchableOpacity disabled={e?.id === tab} onPress={() => setTab(e?.id)} key={e?.id} style={[styles.tab, {backgroundColor: tab === e?.id ? '#f7f9e8' : '#fff'}]}>
+            <Text style={[styles.tab_title, {color: tab === e?.id ? '#111' : '#7f7f7f'}]}>
+              {e?.name}
+            </Text>
+          </TouchableOpacity>
+        ))}
+      </View>
+      
+      {tab_obj?.id === 'Weight_Logs' && <LogsTable data={petData?.weightLogs?.map(e => ({...e, name: `Weight: ${e?.weight}Kg`, date: `Date: ${new Date(e?.date)?.toLocaleDateString()}`}))} />}
+      {tab_obj?.id === 'Body_Condition' && <LogsTable data={petData?.bodyConditionLogs?.map(e => ({...e, name: `${e?.body_condition}`, date: `Date: ${new Date(e?.date)?.toLocaleDateString()}`}))} />}
+      {tab_obj?.id === 'Vet_Visits' && <VetVisitsTable onAddNew={() => {}} data={petData?.vetVisitLogs} />}
     </ScrollView>
   );
 };
@@ -170,6 +218,9 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  scroll_content: {
+    paddingBottom: 30,
   },
   card: {
     padding: 16,
@@ -209,4 +260,48 @@ const styles = StyleSheet.create({
     borderRadius: 8,
     marginBottom: 16,
   },
+  tabs: {
+    marginTop: 5,
+    height: 45,
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderRadius: 8,
+    overflow: 'hidden',
+    borderWidth: .7,
+    borderColor: '#e3e6e8'
+  },
+  tab: {
+    width: '33.5%',
+    height: 45,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#fff'
+  },
+  tab_title: {
+    fontSize: 14,
+    fontWeight: '600',
+  },
+  vet_visit_card: {
+    padding: 8,
+    borderWidth: 1,
+    borderColor: '#eee',
+    marginBottom: 8,
+    borderRadius: 8,
+    gap: 5
+  },
+  button: {
+    height: 40,
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderWidth: .7,
+    borderColor: '#e3e6e8',
+    backgroundColor: '#111',
+    borderRadius: 8,
+    marginTop: 10
+  },
+  button_text: {
+    color: '#fff'
+  }
 }); 
